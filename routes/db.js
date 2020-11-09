8c7247c889eac71f51219a8560ba0bd1f54d9b19
ttubuckee <tommy1003@naver.com>
2020-11-10 08:37:57 +0900

    Add /routes for routing

---
+++ routes/db.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    // db 접속해서 값을 리턴받아서 화에 뿌려주기
+    res.status(200).json({
+        data: [{
+            "name": "김대현",
+            "email": "tommy1003@naver.com",
+            "tel": "01022294267"
+        }, {
+            "name": "크리에이딧",
+            "email": "creadit2020@gmail.com",
+            "tel": "01020202020"
+        }]
+    });
+});
+
+module.exports = router;
