0a43e2e5a43c71c12afe4c4feb823a863cd82747
ttubuckee <tommy1003@naver.com>
2020-11-10 17:41:03 +0900

    Fix popConfirm() to pop_delete_confirm() & Add pop_mod_confirm()

--- public/src/js/register.js
+++ public/src/js/register.js
@@ -14,24 +14,24 @@ function addListenerToItem() {
     }
     return;
 }
-function makeHtmlSyntax(response) {
-    const {data} = response;
-
-    // data는 object 배열
-    if (data[0] === null || data[0] === undefined) {
-        throw new Error("something is going wrong!");
-        return;
-    }
-    const keys = Object.keys(data[0]);
-    const thead = '<table><thead><tr>' + keys.map(e => {
-        return '<th>' + e + '</th>';
-    }).join('') + '</tr></thead>';
-
-    return thead + '<tbody>' + data.map(obj => { // elements 는 obj
-        const elements = Object.keys(obj);
-        return '<tr>' + elements.map(e => {
-            return '<td>' + obj[e] + '</td>';
-        }).join('') + '</tr>';
-    }).join('') + '</tbody></table>';
-    // name, email
-}
+// function makeHtmlSyntax(response) {
+//     const {data} = response;
+//
+//     // data는 object 배열
+//     if (data[0] === null || data[0] === undefined) {
+//         throw new Error("something is going wrong!");
+//         return;
+//     }
+//     const keys = Object.keys(data[0]);
+//     const thead = '<table><thead><tr>' + keys.map(e => {
+//         return '<th>' + e + '</th>';
+//     }).join('') + '</tr></thead>';
+//
+//     return thead + '<tbody>' + data.map(obj => { // elements 는 obj
+//         const elements = Object.keys(obj);
+//         return '<tr>' + elements.map(e => {
+//             return '<td>' + obj[e] + '</td>';
+//         }).join('') + '</tr>';
+//     }).join('') + '</tbody></table>';
+//     // name, email
+// }
